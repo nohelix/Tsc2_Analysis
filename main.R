e848195efbdd701c011efcd30fc4546bc0d83e94
Noelle James <github@nohelix.com>
2022-06-02 12:20:24 -0500

    Overview graph added.
    Suggests that RMS and Amp may vary but Latancy seems boring.

--- main.R
+++ main.R
@@ -30,4 +30,25 @@ Pilot_ABR_data <- read_excel("C:/Users/Noelle/Box/Auerbach Lab (Personal)/Tsc AB
 
 # Graph -------------------------------------------------------------------
 
+# Calculate standard error (SE) like standard deviation (SD)
+se <- function(x, ...) {sqrt(var(x, ...)/length(x))}
+
+
+# Overview Graph ----------------------------------------------------------
+# Graphs everything for an initial check
+
+Pilot_ABR_data %>%
+  # group_by(Rat, Condition, Ear, Freq, dB, Genotype) %>%
+  # summarize(RMS = mean(RMS), `W1 Lat` = mean(`W1 Lat`), `W1 Amp` = mean(`W1 Amp`)) %>%
+  gather(measure, value, RMS, 'W1 Lat', 'W1 Amp') %>%
+  ggplot(aes(x = dB, y = value, color = measure, linetype = Genotype, shape = Genotype, group = interaction(Genotype, measure))) +
+    stat_summary(fun = mean,
+                 fun.min = function(x) mean(x) - se(x),
+                 fun.max = function(x) mean(x) + se(x),
+                 geom = "errorbar", width = 1, position = position_dodge(1)) +
+    stat_summary(fun = mean, geom = "point", position = position_dodge(1), size = 3) +
+    stat_summary(fun = mean, geom = "line") +
+    facet_wrap(~ Freq, scale = "free", nrow = 3, strip.position = "top") +
+    theme_classic()
+
 
