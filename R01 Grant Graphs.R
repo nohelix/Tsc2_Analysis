36c6852864986705b184a399db2ad07d7809a0ab
Noelle James <github@nohelix.com>
2022-06-03 15:04:16 -0500

    Played with layout to match graph.
    Used BBN only for RMS, but chose to use BBN & tones for W1 as it is more representative.

--- R01 Grant Graphs.R
+++ R01 Grant Graphs.R
@@ -1,14 +1,20 @@
+# Select Graphing Data ----------------------------------------------------
+# Can be summarized or not
+
+To_Graph = Pilot_ABR_data_summarized
+
 
 # RMS Grant Graph ---------------------------------------------------------------
-# RMS with color for each Frequency
+# Signal-to-Noise ratio for BBN
 
 To_Graph  %>%
+  filter(Type == "BBN") %>%
   ggplot(aes(x = dB, y = RMS, color = Genotype, group = Genotype)) +
   stat_summary(fun = mean,
                fun.min = function(x) mean(x) - se(x),
                fun.max = function(x) mean(x) + se(x),
-               geom = "errorbar", width = 3, position = position_dodge(1)) +
-  stat_summary(fun = mean, geom = "point", position = position_dodge(1), size = 3) +
+               geom = "errorbar", width = 4, size = 1) +
+  stat_summary(fun = mean, geom = "point", size = 2) +
   stat_summary(fun = mean, geom = "line", size = 1) +
   scale_x_continuous(limits = c(10, 100), breaks = c(20, 40, 60, 80, 100)) +
   scale_color_manual(labels = c("WT" = "Wildtype", "Het" = bquote("Tsc2"^"+/-")),
@@ -18,20 +24,20 @@ To_Graph  %>%
        y = "Signal-to-Noise Ratio (RMS)") +
   theme_classic() +
   theme(
+    text = element_text(size = 12),
     panel.grid.major.x = element_line(color = "white"),
     legend.position = c(0.7, 0.25)
   )
 
-ggsave("R01RMS.jpg",
+ggsave("R01_RMS.jpg",
        plot = last_plot(), # or an explicit ggplot object name
        path = ProjectFolder,
        width = 250, height = 250, units = "px", dpi = 100)
 
 # W1 Grant Graph ---------------------------------------------------------------
-# Wave 1 Amplitude with color for each Frequency
+# Wave 1 Amplitude for all sounds b/c BBN not great.
 
 To_Graph  %>%
-  filter(Type == "BBN") %>%
   ggplot(aes(x = dB, y = `W1 Amp`, color = Genotype, group = Genotype)) +
   stat_summary(fun = mean,
                fun.min = function(x) mean(x) - se(x),
@@ -47,11 +53,12 @@ To_Graph  %>%
        y = "Wave 1 Amplitued") +
   theme_classic() +
   theme(
+    text = element_text(size = 12),
     panel.grid.major.x = element_line(color = "white"),
-    legend.position = c(0.7, 0.25)
+    legend.position = c(0.35, 0.75)
   )
 
-ggsave("R01.jpg",
+ggsave("R01_W1lat.jpg",
        plot = last_plot(), # or an explicit ggplot object name
        path = ProjectFolder,
        width = 250, height = 250, units = "px", dpi = 100)
