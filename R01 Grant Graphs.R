31fc4b5b0b6e6ff1d39753c6eedb8fbcb7d051d7
Noelle James <github@nohelix.com>
2022-06-04 12:02:27 -0500

    added significance tests.

--- R01 Grant Graphs.R
+++ R01 Grant Graphs.R
@@ -1,14 +1,14 @@
 # Select Graphing Data ----------------------------------------------------
 # Can be summarized or not
 
-To_Graph = Pilot_ABR_data_summarized
+To_Graph = Pilot_ABR_data_summarized %>%
+                filter(Type == "BBN")
 
 
 # RMS Grant Graph ---------------------------------------------------------------
-# Signal-to-Noise ratio for BBN
+# Signal-to-Noise ratio for BBN, significant for 70-90dB
 
 To_Graph  %>%
-  filter(Type == "BBN") %>%
   ggplot(aes(x = dB, y = RMS, color = Genotype, group = Genotype)) +
   stat_summary(fun = mean,
                fun.min = function(x) mean(x) - se(x),
@@ -33,6 +33,19 @@ ggsave("R01_RMS.jpg",
        plot = last_plot(), # or an explicit ggplot object name
        path = ProjectFolder,
        width = 250, height = 250, units = "px", dpi = 100)
+
+# RMS ANOVA -------------------------------------------------------------------
+RMS.aov <- aov(RMS ~ Genotype * dB, data = To_Graph)
+summary(RMS.aov)
+TukeyHSD(RMS.aov)
+TukeyHSD(RMS.aov)$`Genotype:dB` %>% 
+  as_tibble(.name_repair = "unique", rownames = "Comparison") %>%  
+  filter(grepl("WT:.*?-Het:.*?|Het:.*?-WT:.*?", Comparison)) %>% 
+  mutate(dB1 = gsub("^.*?WT:(\\d+).*?$","\\1", Comparison),
+         dB2 = gsub("^.*?Het:(\\d+).*?$","\\1", Comparison)) %>%
+  filter(dB1 == dB2) %>%
+  mutate(sig = stars.pval(`p adj`))
+
 
 # W1 Grant Graph ---------------------------------------------------------------
 # Wave 1 Amplitude for BBN to match Fmr1 KO result.
@@ -64,3 +77,26 @@ ggsave("R01_W1lat.jpg",
        plot = last_plot(), # or an explicit ggplot object name
        path = ProjectFolder,
        width = 250, height = 250, units = "px", dpi = 100)
+
+# W1 Amp ANOVA -------------------------------------------------------------------
+W1amp.aov <- aov(`W1 Amp` ~ Genotype * dB, data = To_Graph)
+summary(W1amp.aov)
+TukeyHSD(W1amp.aov)$`Genotype:dB` %>% 
+  as_tibble(.name_repair = "unique", rownames = "Comparison") %>%  
+  filter(grepl("WT:.*?-Het:.*?|Het:.*?-WT:.*?", Comparison)) %>% 
+  mutate(dB1 = gsub("^.*?WT:(\\d+).*?$","\\1", Comparison),
+         dB2 = gsub("^.*?Het:(\\d+).*?$","\\1", Comparison)) %>%
+  filter(dB1 == dB2) %>%
+  mutate(sig = stars.pval(`p adj`))
+
+
+# W1 Lat ANOVA ------------------------------------------------------------------
+W1lat.aov <- aov(`W1 Lat` ~ Genotype * dB, data = To_Graph)
+summary(W1lat.aov)
+TukeyHSD(W1lat.aov)$`Genotype:dB` %>% 
+  as_tibble(.name_repair = "unique", rownames = "Comparison") %>%  
+  filter(grepl("WT:.*?-Het:.*?|Het:.*?-WT:.*?", Comparison)) %>% 
+  mutate(dB1 = gsub("^.*?WT:(\\d+).*?$","\\1", Comparison),
+         dB2 = gsub("^.*?Het:(\\d+).*?$","\\1", Comparison)) %>%
+  filter(dB1 == dB2) %>%
+  mutate(sig = stars.pval(`p adj`))
