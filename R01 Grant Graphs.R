b32b470eff4972222c3dd77be4a6f488b8da0a88
Noelle James <github@nohelix.com>
2022-06-04 10:21:37 -0500

    Improved color match, added proper axis labels and removed the legend background which was covering the lines.

--- R01 Grant Graphs.R
+++ R01 Grant Graphs.R
@@ -18,9 +18,9 @@ To_Graph  %>%
   stat_summary(fun = mean, geom = "line", size = 1) +
   scale_x_continuous(limits = c(10, 100), breaks = c(20, 40, 60, 80, 100)) +
   scale_color_manual(labels = c("WT" = "Wildtype", "Het" = bquote("Tsc2"^"+/-")),
-                     values = c("WT" = "black", "Het" = "blue"),
+                     values = c("WT" = "black", "Het" = "#2832C2"),
                      name = NULL) +
-  labs(x = "Sound Intensity",
+  labs(x = "Sound Intensity (dB)",
        y = "Signal-to-Noise Ratio (RMS)") +
   theme_classic() +
   theme(
@@ -35,9 +35,10 @@ ggsave("R01_RMS.jpg",
        width = 250, height = 250, units = "px", dpi = 100)
 
 # W1 Grant Graph ---------------------------------------------------------------
-# Wave 1 Amplitude for all sounds b/c BBN not great.
+# Wave 1 Amplitude for BBN to match Fmr1 KO result.
 
 To_Graph  %>%
+  # filter(Type == "BBN") %>%
   ggplot(aes(x = dB, y = `W1 Amp`, color = Genotype, group = Genotype)) +
   stat_summary(fun = mean,
                fun.min = function(x) mean(x) - se(x),
@@ -47,15 +48,16 @@ To_Graph  %>%
   stat_summary(fun = mean, geom = "line", size = 1) +
   scale_x_continuous(limits = c(10, 100), breaks = c(20, 40, 60, 80, 100)) +
   scale_color_manual(labels = c("WT" = "Wildtype", "Het" = bquote("Tsc2"^"+/-")),
-                     values = c("WT" = "black", "Het" = "blue"),
+                     values = c("WT" = "black", "Het" = "#2832C2"),
                      name = NULL) +
-  labs(x = "Sound Intensity",
-       y = "Wave 1 Amplitued") +
+  labs(x = "Sound Intensity (dB)",
+       y = expression("Wave 1 Amplitued (\u00b5V)")) +
   theme_classic() +
   theme(
     text = element_text(size = 12),
     panel.grid.major.x = element_line(color = "white"),
-    legend.position = c(0.35, 0.75)
+    legend.position = c(0.3, 0.75),
+    legend.key = element_blank()
   )
 
 ggsave("R01_W1lat.jpg",
