3862233ae09a8531883ab74844217daf5c865635
Noelle James <github@nohelix.com>
2022-09-30 13:31:11 -0500

    Renamed from Analysis workbook to Tsc2 analysis report.
    Renamed from Analysis workbook to Tsc2 analysis report.
    
    Also added hardcoded coloring of Blue to Hets and black to WT

--- Custom Graphs.R
+++ Custom Graphs.R
@@ -240,3 +240,34 @@ Rxn_overall_by_Duration %>%
     plot.title = element_text(hjust = 0.5),
     panel.grid.major.x = element_line(color = rgb(235, 235, 235, 255, maxColorValue = 255))
   )
+
+# Duration vs. Genotype ---------------------------------------------------
+
+Rxn_overall_by_Duration %>%
+  filter(!(Intensity %in% c("90"))) %>%
+  filter(Duration == "Alone") %>%
+  mutate(Genotype = str_extract(Genotype, "Het|WT"),
+         `Dur (ms)` = as.factor(`Dur (ms)`)) %>%
+  ggplot(aes(x = Intensity, y = Rxn)) +
+  # geom_point(aes(group = ID, color = Genotype), alpha = 0.3)+
+  # geom_line(aes(group = ID, color = Genotype), alpha = 0.3)+
+  stat_summary(aes(color = Genotype, linetype = `Dur (ms)`, group = interaction(Genotype, `Dur (ms)`)),
+               fun = mean,
+               fun.min = function(x) mean(x) - se(x),
+               fun.max = function(x) mean(x) + se(x),
+               geom = "errorbar", width = 1, position = position_dodge(0.1)) +
+  stat_summary(aes(color = Genotype, shape = `Dur (ms)`, group = interaction(Genotype, `Dur (ms)`)),
+               fun = mean,
+               geom = "point", position = position_dodge(0.1), size = 3) +
+  stat_summary(aes(color = Genotype, linetype = `Dur (ms)`,, group = interaction(Genotype, `Dur (ms)`)), fun = mean, geom = "line") +
+  labs(title = "Tsc2 Eker",
+       caption = paste("Date:", head(sort(df$Date, decreasing = TRUE))),
+       x = "Intensity (dB)",
+       y = "Reaction time (ms, mean +/- SE)") +
+  scale_x_continuous(breaks = seq(20, 80, by = 10)) +
+  # facet_wrap( ~ `Dur (ms)`, ncol = 1) +
+  theme_classic() +
+  theme(
+    plot.title = element_text(hjust = 0.5),
+    panel.grid.major.x = element_line(color = rgb(235, 235, 235, 255, maxColorValue = 255))
+  )
