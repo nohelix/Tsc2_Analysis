d0e7900a46e7e915dc79b89b34c1cef464e0e425
Noelle James <github@nohelix.com>
2022-10-19 16:53:24 -0500

    Prep for lab meeting

--- Custom Graphs.R
+++ Custom Graphs.R
@@ -241,6 +241,41 @@ Rxn_overall_by_Duration %>%
     panel.grid.major.x = element_line(color = rgb(235, 235, 235, 255, maxColorValue = 255))
   )
 
+
+# Rotating pre vs. post Mix -----------------------------------------------
+
+Rxn_overall_by_Duration %>%
+  filter(!(Intensity %in% c("90"))) %>%
+  filter(ID %in% c("RP 5", "RP 3", "RP 6", "GP 1", "GP 2", "GP 3")) %>%
+  mutate(Genotype = str_extract(Genotype, "Het|WT"),
+         `Stim Length (ms)` = as.factor(`Dur (ms)`),
+         Phase =  case_when(Phase == "BBN Rotating" & ID %in% c("GP 1", "GP 2", "GP 3") ~ "Before",
+                            Phase == "BBN Rotating" & ID %in% c("RP 5", "RP 3", "RP 6") ~ "After",
+                            TRUE ~ ""),
+         Phase = factor(Phase, levels = c("Before", "After", ""))) %>%
+  filter(Phase != "") %>%
+  ggplot(aes(x = Intensity, y = Rxn)) +
+  stat_summary(aes(color = `Stim Length (ms)`, group = `Stim Length (ms)`),
+               fun = mean,
+               fun.min = function(x) mean(x) - se(x),
+               fun.max = function(x) mean(x) + se(x),
+               geom = "errorbar", width = 1, position = position_dodge(0.1)) +
+  stat_summary(aes(shape = `Stim Length (ms)`, color = `Stim Length (ms)`, group = `Stim Length (ms)`),
+               fun = mean,
+               geom = "point", position = position_dodge(0.1), size = 3) +
+  stat_summary(aes(color = `Stim Length (ms)`, group = `Stim Length (ms)`), fun = mean, geom = "line") +
+  labs(title = "Order of presentation for rotating durations",
+       caption = paste("Date:", head(df$Date)),
+       x = "Intensity (dB)",
+       y = "Mean Reaction time (ms)") +
+  scale_x_continuous(breaks = seq(20, 80, by = 10)) +
+  facet_wrap( ~ Phase, ncol = 2) +
+  theme_classic() +
+  theme(
+    plot.title = element_text(hjust = 0.5),
+    panel.grid.major.x = element_line(color = rgb(235, 235, 235, 255, maxColorValue = 255))
+  )
+
 # Duration vs. Genotype ---------------------------------------------------
 
 Rxn_overall_by_Duration %>%
@@ -270,4 +305,67 @@ Rxn_overall_by_Duration %>%
   theme(
     plot.title = element_text(hjust = 0.5),
     panel.grid.major.x = element_line(color = rgb(235, 235, 235, 255, maxColorValue = 255))
+  )
+
+
+Rxn_overall_by_Duration %>%
+  filter(!(Intensity %in% c("90"))) %>%
+  # filter(Duration == "Alone") %>%
+  mutate(Genotype = str_extract(Genotype, "Het|WT"),
+         `Dur (ms)` = as.factor(`Dur (ms)`)) %>%
+  ggplot(aes(x = Intensity, y = Rxn)) +
+  stat_summary(aes(color = Genotype, linetype = `Dur (ms)`, group = interaction(Genotype, `Dur (ms)`)),
+               fun = mean,
+               fun.min = function(x) mean(x) - se(x),
+               fun.max = function(x) mean(x) + se(x),
+               geom = "errorbar", width = 1, position = position_dodge(0.1)) +
+  stat_summary(aes(color = Genotype, shape = `Dur (ms)`, group = interaction(Genotype, `Dur (ms)`)),
+               fun = mean,
+               geom = "point", position = position_dodge(0.1), size = 3) +
+  stat_summary(aes(color = Genotype, linetype = `Dur (ms)`,, group = interaction(Genotype, `Dur (ms)`)), fun = mean, geom = "line") +
+  scale_color_manual(labels = c("WT" = "Wildtype", "Het" = bquote("Tsc2"^"+/-")),
+                     values = c("WT" = "black", "Het" = "deepskyblue")) +
+  labs(title = "Tsc2 Eker",
+       caption = paste("Date:", head(sort(df$Date, decreasing = TRUE))),
+       x = "Intensity (dB)",
+       y = "Reaction time (ms, mean +/- SE)") +
+  scale_x_continuous(breaks = seq(20, 80, by = 10)) +
+  theme_classic() +
+  theme(
+    plot.title = element_text(hjust = 0.5),
+    panel.grid.major.x = element_line(color = rgb(235, 235, 235, 255, maxColorValue = 255))
+  )
+
+
+# Holder ------------------------------------------------------------------
+
+Rxn_overall_by_Duration %>%
+  filter(!(Intensity %in% c("90"))) %>%
+  filter(Duration == "Mix") %>%
+  filter(!(ID %in% partial.data)) %>%
+  mutate(Genotype = str_extract(Genotype, "Het|WT")) %>%
+  ggplot(aes(x = Intensity, y = Rxn)) +
+  stat_summary(aes(color = Genotype, group = Genotype),
+               fun = mean,
+               fun.min = function(x) mean(x) - se(x),
+               fun.max = function(x) mean(x) + se(x),
+               geom = "errorbar", width = 1, position = position_dodge(0.1)) +
+  stat_summary(aes(color = Genotype, group = Genotype),
+               fun = mean,
+               geom = "point", position = position_dodge(0.1), size = 3) +
+  stat_summary(aes(color = Genotype, group = Genotype), fun = mean, geom = "line") +
+  scale_color_manual(labels = c("WT" = "Wildtype", "Het" = bquote("Tsc2"^"+/-")),
+                     values = c("WT" = "black", "Het" = "deepskyblue")) +
+  labs(title = "Tsc2 Eker (Mix duration)",
+       caption = paste("Date:", head(sort(df$Date, decreasing = TRUE))),
+       x = "Intensity (dB)",
+       y = "Reaction time (ms, mean +/- SE)") +
+  scale_x_continuous(breaks = seq(20, 80, by = 10)) +
+  facet_wrap( ~ `Dur (ms)`, ncol = 1) +
+  theme_classic() +
+  theme(
+    plot.title = element_text(hjust = 0.5),
+    panel.grid.major.x = element_line(color = rgb(235, 235, 235, 255, maxColorValue = 255)),
+    legend.position = c(0.87, 0.2),
+    legend.key = element_blank()
   )
